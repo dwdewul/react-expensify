291b43f022ef04ce9383aa32fe6e2afba6b84391
David DeWulf <dwdewul@gmail.com>
2017-11-01 13:04:18 -0500

    Created simple Expres server to hoist app Removed init data for app Removed Edit Expense navlink from header

--- src/app.js
+++ src/app.js
@@ -13,13 +13,8 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water Bill', amount: 45000 }));
-store.dispatch(addExpense({ description: 'Gas Bill', amount: 10000, createdAt: 3000 }));
-store.dispatch(addExpense({ description: 'Rent', amount: 109500 }));
-
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+// const state = store.getState();
+// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 
 
 const jsx = (
