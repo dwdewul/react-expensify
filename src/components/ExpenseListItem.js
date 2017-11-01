d82f00d9ff1e791260332a89bd221b90aed0cd17
David DeWulf <dwdewul@gmail.com>
2017-11-01 14:11:07 -0500

    Added numeral and changed date output for ExpenseListItem

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
 const ExpenseListItem = ({ dispatch, id, description, amount, createdAt }) => {
     return (
         <div>
             <Link to={`/edit/${id}`}><h3>{description}</h3></Link>
-            <p>{amount}</p>
-            <p>{createdAt}</p>
+            <p>{numeral(amount / 100).format('$0,0.00')}</p>
+            <p>{moment(createdAt).format('MMMM Do, YYYY')}</p>
             
         </div>
     );
